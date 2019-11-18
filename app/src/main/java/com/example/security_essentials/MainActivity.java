f230cbf3fb078bd87845353bb282116bf203b3ba
alejandroesalas <alejandroesalas@gmail.com>
2019-11-18 16:46:41 -0500

    Conexion a Firebase

--- app/src/main/java/com/example/security_essentials/MainActivity.java
+++ app/src/main/java/com/example/security_essentials/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.security_essentials;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+if(currentUser!=null){
+
+}else{
+
+}
+
     }
 }
